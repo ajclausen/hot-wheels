31588f7d1907af8adebf61959342aa4c615bb3e5
Andrew <andrew@clausen.app>
2024-11-12 15:00:45 -0500

    bugfix

--- src/components/InventoryView.tsx
+++ src/components/InventoryView.tsx
@@ -79,7 +79,7 @@ export function InventoryView({ models, onToggleOwned, onEditNotes, onOpenSearch
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
             {filteredModels.map(model => (
               <ModelCard
                 key={model.id}
